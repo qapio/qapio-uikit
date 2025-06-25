efe54df1d0e06b61b71575764aff8d76e66d0daa
Fridtjof Kjelstrup <fridtjof.kjelstrup@qapio.com>
2025-06-25 16:04:40 +0200

    * Add Scroll Area

--- src/components/index.ts
+++ src/components/index.ts
@@ -13,6 +13,7 @@ export * from "./ui/form";
 export * from "./ui/glowing-effect";
 export * from "./ui/input";
 export * from "./ui/label";
+export * from "./ui/scroll-area";
 export * from "./ui/select";
 export * from "./ui/separator";
 export * from "./ui/sheet";
