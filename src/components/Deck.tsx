1c2c6beaf5dec1b96a3af3b70d36d790537651f6
Fridtjof Kjelstrup <fridtjof.kjelstrup@qapio.com>
2025-09-05 14:58:18 +0200

    * Add Deck

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -1,20 +1,24 @@
 import {AnimatePresence, motion} from "motion/react";
+import * as React from "react";
 
 export const Deck = ({items, active}) => {
+
+    const [selected, setSelected] = React.useState(active);
 
     const randomRotateY = () => {
         return Math.floor(Math.random() * 21) - 10;
     };
 
     const isActive = (index: number) => {
-        return index === active;
+        return index === selected;
     };
 
     return (
-        <div className="relative h-full w-full bg-yellow-600">
+        <div className="relative h-full w-full">
             <AnimatePresence>
                 {items.map((item, index) => (
                     <motion.div
+                       // onClick={() => setSelected((selected+1) % items.length)}
                         key={index}
                         initial={{
                             opacity: 0,
